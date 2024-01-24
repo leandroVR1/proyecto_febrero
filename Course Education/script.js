0229a550dfe38411b157c3e50aa7611fefc7c544
harlinsonvasquez <harvack25@gmail.com>
2024-01-23 19:05:28 -0500

    esto es una prueba

--- Course Education/script.js
+++ Course Education/script.js
@@ -5,5 +5,6 @@ btn.addEventListener("click", e => {
     menu.classList.toggle("menu-expanded");
     menu.classList.toggle("menu-collapsed")
 })
+//esto es un comentario
 
 document.getElementById("body").classList.toggle("body-expanded");
