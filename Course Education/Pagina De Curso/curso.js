b4609d59ef1880949261acd5508adb81d32f3421
leandroVR1 <149026211+leandroVR1@users.noreply.github.com>
2024-01-29 17:45:12 -0500

    Delete Course Education directory

--- Course Education/Pagina De Curso/curso.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("menu-btn");
-const menu = document.getElementById("sidemenu");
-
-btn.addEventListener("click", e => {
-    menu.classList.toggle("menu-expanded");
-    menu.classList.toggle("menu-collapsed")
-})
-
-document.getElementById("body").classList.toggle("body-expanded");
