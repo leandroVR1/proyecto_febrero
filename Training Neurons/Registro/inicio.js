e6b97183bc268f098ab24f109429675a6efaed9f
riwip5 <none>
2024-01-30 21:36:22 -0500

    localstorage

--- Training Neurons/Registro/inicio.js
+++ Training Neurons/Registro/inicio.js
@@ -1,7 +1,16 @@
+// Función para generar un ID único
+function generateUniqueId() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+// Espera a que se cargue el DOM antes de ejecutar el código
 document.addEventListener('DOMContentLoaded', function () {
+
+  // Agrega un evento al formulario de registro
   document.getElementById('registroForm').addEventListener('submit', function (event) {
     event.preventDefault(); // Evita que el formulario se envíe de manera convencional
-
+    
+    // Obtiene los valores de los campos del formulario
     var name = document.getElementById('name').value;
     var lastname = document.getElementById('lastname').value;
     var phone = document.getElementById('phone').value;
@@ -15,14 +24,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-
+  
     // Verifica si al menos uno de los campos requeridos está vacío
     if (!name || !lastname || !phone || !birthdate || !country || !city || !gender || !email || !password) {
       alert('Por favor, completa todos los campos del formulario.');
       return; // Evita continuar con la solicitud si hay campos vacíos
     }
 
+    // Crea un objeto con los datos del usuario
     var userData = {
+      id: generateUniqueId(),
       name: name,
       lastname: lastname,
       phone: phone,
@@ -33,24 +44,28 @@ document.addEventListener('DOMContentLoaded', function () {
       email: email,
       password: password
     };
+    
+    // Guarda el usuario en localStorage
+    localStorage.setItem("user", JSON.stringify(userData));
 
-    fetch('http://192.168.21.165:3000/users', {
+    // Envía los datos del usuario al servidor
+    fetch('http://192.168.21.165:3000/users',  {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(userData)
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Respuesta del servidor:', data);
-        sessionStorage.setItem("nombre", name);
-        // Si los datos se envían correctamente al servidor, realiza la redirección
-        window.location.href = "http://127.0.0.1:5500/Logeado/index.html";
-      })
-      .catch(error => {
-        console.error('Error al enviar datos:', error);
-
-      });
+    .then(response => response.json())
+    .then(data => {
+      console.log('Respuesta del servidor:', data);
+      
+      // Si los datos se envían correctamente al servidor, realiza la redirección
+      window.location.href = "http://127.0.0.1:5501/Training%20Neurons/Logeado/index.html";
+    })
+    .catch(error => {
+      console.error('Error al enviar datos:', error);
+      // Maneja el error de manera adecuada
+    });
   });
 });
