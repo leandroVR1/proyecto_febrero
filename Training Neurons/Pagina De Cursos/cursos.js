02352dd68e0fb29e48a41856d93c69f30c6faad8
harlinsonvasquez <harvack25@gmail.com>
2024-01-31 20:30:44 -0500

    avance

---
+++ Training Neurons/Pagina De Cursos/cursos.js
@@ -0,0 +1,73 @@
+let root=document.getElementById('root');
+var container=document.createElement('div');
+container.classList.add('container');
+root.appendChild(container);
+
+var row=document.createElement('div');
+row.classList.add('row','mt-5');
+container.appendChild(row)
+
+let apiCursos="http://localhost:3000/categories";
+let datos=[];
+
+function cargarDatos(){
+    fetch(apiCursos)
+    .then(response=>response.json())
+    .then(data=>{
+        datos=data;
+        cargarTargetas();
+    })
+    .catch(error=>console.error("error al cargar los datos", error));
+}
+
+function cargarTargetas(){
+
+
+    datos.forEach((dato)=>{
+        console.log(dato.img)
+        let col=document.createElement('div')
+        col.classList.add('col-md-3');
+        row.appendChild(col);
+
+        let card=document.createElement('div')
+        card.classList.add('card','mb-4')
+        col.appendChild(card)
+
+        let img=document.createElement('img')
+        img.classList.add('img-fluid')
+        img.setAttribute('src',`${dato.img}`);
+        card.appendChild(img)
+        
+
+        let cardbody=document.createElement('div')
+        cardbody.classList.add('card-body')
+        card.appendChild(cardbody)
+
+        let h2=document.createElement('h2')
+        h2.innerText=dato.title
+        cardbody.appendChild(h2)
+
+        let p=document.createElement('p')
+        p.innerText=dato.description;
+        cardbody.appendChild(p);
+
+        let button = document.createElement("button");
+        button.classList.add('btn', 'btn-danger', 'w-100', 'mt-4');
+        button.style.backgroundColor = "red";
+        button.style.border = "none";
+        button.innerText = "Ir al curso";
+
+        button.onclick=function(){
+            redirigir(dato.website);
+            console.log(dato.website)
+        }
+
+        cardbody.appendChild(button);
+        container.appendChild(row);
+    })
+
+}
+function redirigir(url) {
+    window.open(url, "_blank");
+}
+cargarDatos();
