863d65eda7d3c7187db4c2b6799174a967f96edc
harlinsonvasquez <harvack25@gmail.com>
2024-02-02 17:48:21 -0500

    creacionn de cursos reales

--- Training Neurons/Pagina De Cursos/cursos.js
+++ Training Neurons/Pagina De Cursos/cursos.js
@@ -1,93 +1,94 @@
-let root=document.getElementById('root');
-var container=document.createElement('div');
+let root = document.getElementById('root');
+var container = document.createElement('div');
 container.classList.add('container2');
 root.appendChild(container);
 
 
 
-var row=document.createElement('div');
-row.classList.add('row','mt-5');
+var row = document.createElement('div');
+row.classList.add('row', 'mt-5');
 container.appendChild(row)
 
-let apiCursos="http://localhost:3000/categories";
-let datos=[];
+let apiCursos = "http://localhost:3000/categories";
+let datos = [];
 
-function cargarDatos(){
+function cargarDatos() {
     fetch(apiCursos)
-    .then(response=>response.json())
-    .then(data=>{
-        datos=data;
-        cargarTargetas();
-    })
-    .catch(error=>console.error("error al cargar los datos", error));
+        .then(response => response.json())
+        .then(data => {
+            datos = data;
+            cargarTargetas();
+        })
+        .catch(error => console.error("error al cargar los datos", error));
 }
 
-function cargarTargetas(){
-    let idCurso =localStorage.getItem('cursos');//codigo para traer lo que esta guardado en el local 
+function cargarTargetas() {
+    let idCurso = localStorage.getItem('cursos');//codigo para traer lo que esta guardado en el local 
 
 
-    filtro = datos.filter(function(dato){
-        return dato.categoria ==idCurso;
+    filtro = datos.filter(function (dato) {
+        return dato.categoria == idCurso;
     });
 
-    filtro.forEach((dato)=>{
-        
-        let col=document.createElement('div')
+    filtro.forEach((dato) => {
+
+        let col = document.createElement('div')
         col.classList.add('col-md-4');
         row.appendChild(col);
 
-        let card=document.createElement('div')
-        card.classList.add('card','mb-4')
+        let card = document.createElement('div')
+        card.classList.add('card')
+        card.style.marginRight = '20px';
         col.appendChild(card)
 
-        let content=document.createElement('div')
+        let content = document.createElement('div')
         content.classList.add("content")
         card.appendChild(content)
 
-        let encabezado=document.createElement('div')
+        let encabezado = document.createElement('div')
         encabezado.classList.add('encabezado')
         content.appendChild(encabezado)
 
-        let icono=document.createElement('div')
+        let icono = document.createElement('div')
         icono.classList.add('icono')
         encabezado.appendChild(icono)
 
-        let i=document.createElement('i')
-        i.classList.add('fa-solid','fa-star')
-        i.style.color='#f6d32d';
+        let i = document.createElement('i')
+        i.classList.add('fa-solid', 'fa-star')
+        i.style.color = '#f6d32d';
         icono.appendChild(i)
 
-        let categoria=document.createElement('div')
+        let categoria = document.createElement('div')
         categoria.classList.add('categoria')
-        categoria.id=('categoria')
+        categoria.id = ('categoria')
         encabezado.appendChild(categoria)
 
-        let label=document.createElement('label')
-        label.innerText=dato.categoria;
-        label.setAttribute('for','categoria')
-        categoria.appendChild(label); 
+        let label = document.createElement('label')
+        label.innerText = dato.categoria;
+        label.setAttribute('for', 'categoria')
+        categoria.appendChild(label);
 
-        let title=document.createElement('div')
+        let title = document.createElement('div')
         title.classList.add('titulo')
         content.appendChild(title)
 
-        let h2=document.createElement('h2')
-        h2.innerText=dato.title
+        let h2 = document.createElement('h2')
+        h2.innerText = dato.title
         title.appendChild(h2)
 
-        let description=document.createElement('div')
+        let description = document.createElement('div')
         description.classList.add('descripcion')
         content.appendChild(description)
 
-        let p=document.createElement('p')
-        p.innerText=dato.description;
+        let p = document.createElement('p')
+        p.innerText = dato.description;
         description.appendChild(p);
 
-        let separacion=document.createElement('div')
+        let separacion = document.createElement('div')
         separacion.classList.add('separacion')
         content.appendChild(separacion);
 
-        let pieTarjeta=document.createElement('div')
+        let pieTarjeta = document.createElement('div')
         pieTarjeta.classList.add('pietarjeta')
         content.appendChild(pieTarjeta)
 
@@ -98,25 +99,25 @@ function cargarTargetas(){
         button.innerText = "Ir al curso";
         pieTarjeta.appendChild(button);
 
-        let icono2=document.createElement('div')
+        let icono2 = document.createElement('div')
         icono2.classList.add('icono2')
         pieTarjeta.appendChild(icono2)
 
 
-        let img=document.createElement('img')
+        let img = document.createElement('img')
         img.classList.add('img-fluid')
-        img.setAttribute('src',`${dato.img}`);
+        img.setAttribute('src', `${dato.img}`);
         icono2.appendChild(img)
 
-       
 
 
-        button.onclick=function(){
+
+        button.onclick = function () {
             redirigir(dato.website);
-            
+
         }
 
-        
+
         container.appendChild(row);
     })
 
